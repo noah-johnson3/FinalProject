745ebff803cf5dd4406582ca79db0f0ec02f3b95
noah-johnson3 <noah.johnson3@yahoo.com>
2022-06-22 12:03:16 -0400

    Create Boot REST project, set up JPA Project dependency

---
+++ NewVisionNutrition/src/main/java/com/skilldistillery/newvision/NewVisionNutritionApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.newvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewVisionNutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewVisionNutritionApplication.class, args);
+	}
+
+}
---
+++ NewVisionNutrition/src/test/java/com/skilldistillery/newvision/NewVisionNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.newvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewVisionNutritionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

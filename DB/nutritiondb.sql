0e25d733a47d6d46f1f923db0fb413d74a652403
Katherine <remick.ka@gmail.com>
2022-06-22 14:15:51 -0700

    database approved, test data added

--- DB/nutritiondb.sql
+++ DB/nutritiondb.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `height` DECIMAL(5,2) NOT NULL,
   `weight` DECIMAL(5,2) NOT NULL,
-  `age` INT NOT NULL,
+  `date_of_birth` DATE NOT NULL,
   `active` TINYINT NOT NULL,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
@@ -59,6 +59,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` TEXT NOT NULL,
   `team_id` INT NULL,
   `role` VARCHAR(45) NOT NULL,
+  `create_at` DATETIME NOT NULL,
+  `image_url` TEXT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_user_gender1_idx` (`gender_id` ASC),
   INDEX `fk_user_team1_idx` (`team_id` ASC),
@@ -87,6 +89,9 @@ CREATE TABLE IF NOT EXISTS `goal` (
   `weight` DECIMAL(5,2) NULL,
   `achieved` TINYINT NOT NULL,
   `user_id` INT NOT NULL,
+  `description` VARCHAR(200) NULL,
+  `date_achieved` DATE NULL,
+  `created_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_Goal_User_idx` (`user_id` ASC),
   CONSTRAINT `fk_Goal_User`
@@ -104,11 +109,11 @@ DROP TABLE IF EXISTS `blog` ;
 
 CREATE TABLE IF NOT EXISTS `blog` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `topic` VARCHAR(45) NULL,
-  `text` TEXT NOT NULL,
+  `content` TEXT NOT NULL,
   `image_link` TEXT NULL,
   `user_id` INT NOT NULL,
   `created_at` DATETIME NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_blog_User1_idx` (`user_id` ASC),
   CONSTRAINT `fk_blog_User1`
@@ -126,9 +131,10 @@ DROP TABLE IF EXISTS `comment` ;
 
 CREATE TABLE IF NOT EXISTS `comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(200) NOT NULL,
+  `content` VARCHAR(200) NOT NULL,
   `blog_id` INT NOT NULL,
   `user_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_comment_blog1_idx` (`blog_id` ASC),
   INDEX `fk_comment_User1_idx` (`user_id` ASC),
@@ -152,33 +158,16 @@ DROP TABLE IF EXISTS `tracked_day` ;
 
 CREATE TABLE IF NOT EXISTS `tracked_day` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NOT NULL,
+  `day` DATE NOT NULL,
   `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `date_UNIQUE` (`date` ASC),
+  UNIQUE INDEX `date_UNIQUE` (`day` ASC),
   INDEX `fk_tracked_day_User1_idx` (`user_id` ASC),
   CONSTRAINT `fk_tracked_day_User1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `nutrition`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `nutrition` ;
-
-CREATE TABLE IF NOT EXISTS `nutrition` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `protein` DECIMAL(5,2) NULL,
-  `carbs` DECIMAL(5,2) NULL,
-  `fats` DECIMAL(5,2) NULL,
-  `sodium` INT NULL,
-  `sugar` INT NULL,
-  `calories` DECIMAL(6,2) NULL,
-  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -195,38 +184,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `meal`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `meal` ;
-
-CREATE TABLE IF NOT EXISTS `meal` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `tracked_day_id` INT NOT NULL,
-  `nutrition_id` INT NOT NULL,
-  `meal_type_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_meal_tracked_day1_idx` (`tracked_day_id` ASC),
-  INDEX `fk_meal_nutrition1_idx` (`nutrition_id` ASC),
-  INDEX `fk_meal_meal_type1_idx` (`meal_type_id` ASC),
-  CONSTRAINT `fk_meal_tracked_day1`
-    FOREIGN KEY (`tracked_day_id`)
-    REFERENCES `tracked_day` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_meal_nutrition1`
-    FOREIGN KEY (`nutrition_id`)
-    REFERENCES `nutrition` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_meal_meal_type1`
-    FOREIGN KEY (`meal_type_id`)
-    REFERENCES `meal_type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `recipe`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `recipe` ;
@@ -234,15 +191,56 @@ DROP TABLE IF EXISTS `recipe` ;
 CREATE TABLE IF NOT EXISTS `recipe` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `link` TEXT NULL,
-  `time` INT NULL,
+  `time_required` INT NULL,
   `recipe_text` TEXT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `nutrition_id` INT NOT NULL,
+  `protein` DECIMAL(5,2) NULL,
+  `carbs` DECIMAL(5,2) NULL,
+  `fats` DECIMAL(5,2) NULL,
+  `sodium` INT NULL,
+  `sugar` INT NULL,
+  `calories` DECIMAL(6,2) NULL,
+  `user_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  `image_url` TEXT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_recipe_nutrition1_idx` (`nutrition_id` ASC),
-  CONSTRAINT `fk_recipe_nutrition1`
-    FOREIGN KEY (`nutrition_id`)
-    REFERENCES `nutrition` (`id`)
+  INDEX `fk_recipe_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_recipe_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `meal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `meal` ;
+
+CREATE TABLE IF NOT EXISTS `meal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tracked_day_id` INT NOT NULL,
+  `meal_type_id` INT NOT NULL,
+  `recipe_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_meal_tracked_day1_idx` (`tracked_day_id` ASC),
+  INDEX `fk_meal_meal_type1_idx` (`meal_type_id` ASC),
+  INDEX `fk_meal_recipe1_idx` (`recipe_id` ASC),
+  CONSTRAINT `fk_meal_tracked_day1`
+    FOREIGN KEY (`tracked_day_id`)
+    REFERENCES `tracked_day` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_meal_meal_type1`
+    FOREIGN KEY (`meal_type_id`)
+    REFERENCES `meal_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_meal_recipe1`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `recipe` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -268,40 +266,21 @@ DROP TABLE IF EXISTS `forum_post` ;
 CREATE TABLE IF NOT EXISTS `forum_post` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `topic` VARCHAR(45) NULL,
-  `text` TEXT NULL,
+  `content` TEXT NULL,
   `user_id` INT NOT NULL,
   `created_at` DATETIME NOT NULL,
+  `in_reply_to_id` INT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_forum_post_User1_idx` (`user_id` ASC),
+  INDEX `fk_forum_post_forum_post1_idx` (`in_reply_to_id` ASC),
   CONSTRAINT `fk_forum_post_User1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `reply`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `reply` ;
-
-CREATE TABLE IF NOT EXISTS `reply` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` TEXT NOT NULL,
-  `forum_post_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_reply_forum_post1_idx` (`forum_post_id` ASC),
-  INDEX `fk_reply_User1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_reply_forum_post1`
-    FOREIGN KEY (`forum_post_id`)
-    REFERENCES `forum_post` (`id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_reply_User1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
+  CONSTRAINT `fk_forum_post_forum_post1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `forum_post` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -334,7 +313,14 @@ CREATE TABLE IF NOT EXISTS `exercise_day` (
   `wokout` VARCHAR(45) NULL,
   `calories` INT NULL,
   `exercised` TINYINT NULL,
-  PRIMARY KEY (`id`))
+  `tracked_day_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_exercise_day_tracked_day1_idx` (`tracked_day_id` ASC),
+  CONSTRAINT `fk_exercise_day_tracked_day1`
+    FOREIGN KEY (`tracked_day_id`)
+    REFERENCES `tracked_day` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -441,8 +427,7 @@ DROP TABLE IF EXISTS `chat` ;
 
 CREATE TABLE IF NOT EXISTS `chat` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(200) NOT NULL,
-  `chat_id` INT NOT NULL,
+  `content` VARCHAR(200) NOT NULL,
   `user_id` INT NOT NULL,
   `created_at` DATETIME NOT NULL,
   `team_id` INT NOT NULL,
@@ -457,6 +442,43 @@ CREATE TABLE IF NOT EXISTS `chat` (
   CONSTRAINT `fk_chat_team1`
     FOREIGN KEY (`team_id`)
     REFERENCES `team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `topic`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `topic` ;
+
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `blog_topic`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blog_topic` ;
+
+CREATE TABLE IF NOT EXISTS `blog_topic` (
+  `topic_id` INT NOT NULL,
+  `blog_id` INT NOT NULL,
+  PRIMARY KEY (`topic_id`, `blog_id`),
+  INDEX `fk_topic_has_blog_blog1_idx` (`blog_id` ASC),
+  INDEX `fk_topic_has_blog_topic1_idx` (`topic_id` ASC),
+  CONSTRAINT `fk_topic_has_blog_topic1`
+    FOREIGN KEY (`topic_id`)
+    REFERENCES `topic` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_topic_has_blog_blog1`
+    FOREIGN KEY (`blog_id`)
+    REFERENCES `blog` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -500,9 +522,9 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `user` (`id`, `height`, `weight`, `age`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`) VALUES (1, 67, 150, 27, 1, 'Katherine', 'Remick', 'remick.ka@gmail.com', 1, 'kate', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_ADMIN');
-INSERT INTO `user` (`id`, `height`, `weight`, `age`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`) VALUES (2, 65, 145, 25, 1, 'Kiera', 'Metz', 'kmetz@witcher.game', 1, 'kiera', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_STANDARD');
-INSERT INTO `user` (`id`, `height`, `weight`, `age`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`) VALUES (3, 74, 200, 60, 1, 'Geralt', 'of Rivia', 'grivia@witcher.game', 2, 'geralt', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_STANDARD');
+INSERT INTO `user` (`id`, `height`, `weight`, `date_of_birth`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`, `create_at`, `image_url`) VALUES (1, 67, 150, '1995-03-21 ', 1, 'Katherine', 'Remick', 'remick.ka@gmail.com', 1, 'kate', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_ADMIN', '2022-06-21 03:20:20', NULL);
+INSERT INTO `user` (`id`, `height`, `weight`, `date_of_birth`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`, `create_at`, `image_url`) VALUES (2, 65, 145, '1100-04-05', 1, 'Kiera', 'Metz', 'kmetz@witcher.game', 1, 'kiera', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_STANDARD', '2022-06-21 03:20:20', NULL);
+INSERT INTO `user` (`id`, `height`, `weight`, `date_of_birth`, `active`, `first_name`, `last_name`, `email`, `gender_id`, `username`, `password`, `team_id`, `role`, `create_at`, `image_url`) VALUES (3, 74, 200, '1050-7-12', 1, 'Geralt', 'of Rivia', 'grivia@witcher.game', 2, 'geralt', '$2a$10$BksOLP0ikdH575YcbXXmIOpDYCbuoULmmTLWkQ8galzRaiC3PWpgC', 1, 'ROLE_STANDARD', '2022-06-21 03:20:20', NULL);
 
 COMMIT;
 
@@ -512,7 +534,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `goal` (`id`, `name`, `weight`, `achieved`, `user_id`) VALUES (1, 'Eat Grass', 200, 0, 3);
+INSERT INTO `goal` (`id`, `name`, `weight`, `achieved`, `user_id`, `description`, `date_achieved`, `created_at`) VALUES (1, 'Eat Grass', 200, 0, 3, NULL, NULL, '2022-06-21 03:20:20');
 
 COMMIT;
 
@@ -522,7 +544,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `blog` (`id`, `topic`, `text`, `image_link`, `user_id`, `created_at`) VALUES (1, 'Test Blog', 'This is a test CAN YOU HEAR ME FROM IN HERE?', NULL, 1, '2022-06-21 20:51:21');
+INSERT INTO `blog` (`id`, `content`, `image_link`, `user_id`, `created_at`, `title`) VALUES (1, 'This is a test CAN YOU HEAR ME FROM IN HERE?', NULL, 1, '2022-06-21 20:51:21', 'Blog 1 TEST');
 
 COMMIT;
 
@@ -532,8 +554,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `comment` (`id`, `text`, `blog_id`, `user_id`) VALUES (1, 'YES I CAN HEAR YOU - WHY ARE YOU IN MY COMPUTER???!', 1, 1);
-INSERT INTO `comment` (`id`, `text`, `blog_id`, `user_id`) VALUES (2, 'WHaT iS a COmpUTER?', 1, 3);
+INSERT INTO `comment` (`id`, `content`, `blog_id`, `user_id`, `created_at`) VALUES (1, 'YES I CAN HEAR YOU - WHY ARE YOU IN MY COMPUTER???!', 1, 1, '2022-06-21 08:20:20');
+INSERT INTO `comment` (`id`, `content`, `blog_id`, `user_id`, `created_at`) VALUES (2, 'WHaT iS a COmpUTER?', 1, 3, '2022-06-21 08:20:20');
 
 COMMIT;
 
@@ -543,18 +565,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `tracked_day` (`id`, `date`, `user_id`) VALUES (1, '2022-06-21 21:00:00', 3);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `nutrition`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `nutritiondb`;
-INSERT INTO `nutrition` (`id`, `protein`, `carbs`, `fats`, `sodium`, `sugar`, `calories`) VALUES (1, 40, 5, 10, 200, 3, 800);
-INSERT INTO `nutrition` (`id`, `protein`, `carbs`, `fats`, `sodium`, `sugar`, `calories`) VALUES (2, 30, 20, 5, 50, 0, 300);
+INSERT INTO `tracked_day` (`id`, `day`, `user_id`) VALUES (1, '2022-06-21', 3);
 
 COMMIT;
 
@@ -573,23 +584,23 @@ COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `meal`
+-- Data for table `recipe`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `meal` (`id`, `tracked_day_id`, `nutrition_id`, `meal_type_id`) VALUES (1, 1, 1, 1);
-INSERT INTO `meal` (`id`, `tracked_day_id`, `nutrition_id`, `meal_type_id`) VALUES (2, 1, 1, 2);
-INSERT INTO `meal` (`id`, `tracked_day_id`, `nutrition_id`, `meal_type_id`) VALUES (3, 1, 1, 3);
+INSERT INTO `recipe` (`id`, `link`, `time_required`, `recipe_text`, `name`, `protein`, `carbs`, `fats`, `sodium`, `sugar`, `calories`, `user_id`, `created_at`, `updated_at`, `image_url`) VALUES (1, NULL, 10, '2 Eggs, 1 cup frozen hash brown mix, 1 tbsp vegetable oil: Add oil and hashbrowns to pan, set to medium heat. Beat eggs, add seasoning to taste. When hashbrowns begin to brown add in eggs. Stir and serve when at desired firmness.', 'Easy Morning Hash', 20, 10, 9, 20, 0, 300, 1, '2022-06-21 03:20:20', NULL, NULL);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `recipe`
+-- Data for table `meal`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `recipe` (`id`, `link`, `time`, `recipe_text`, `name`, `nutrition_id`) VALUES (1, NULL, 10, '2 Eggs, 1 cup frozen hash brown mix, 1 tbsp vegetable oil: Add oil and hashbrowns to pan, set to medium heat. Beat eggs, add seasoning to taste. When hashbrowns begin to brown add in eggs. Stir and serve when at desired firmness.', 'Easy Morning Hash', 2);
+INSERT INTO `meal` (`id`, `tracked_day_id`, `meal_type_id`, `recipe_id`) VALUES (1, 1, 1, 1);
+INSERT INTO `meal` (`id`, `tracked_day_id`, `meal_type_id`, `recipe_id`) VALUES (2, 1, 2, 1);
+INSERT INTO `meal` (`id`, `tracked_day_id`, `meal_type_id`, `recipe_id`) VALUES (3, 1, 3, 1);
 
 COMMIT;
 
@@ -611,7 +622,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `forum_post` (`id`, `topic`, `text`, `user_id`, `created_at`) VALUES (1, 'Tests', 'THIS IS A TEST... Your exam will begin now. If you see this - you passed', 1, '2022-06-21 21:00:00');
+INSERT INTO `forum_post` (`id`, `topic`, `content`, `user_id`, `created_at`, `in_reply_to_id`) VALUES (1, 'Tests', 'THIS IS A TEST... Your exam will begin now. If you see this - you passed', 1, '2022-06-21 21:00:00', NULL);
 
 COMMIT;
 
@@ -644,8 +655,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `nutritiondb`;
-INSERT INTO `chat` (`id`, `text`, `chat_id`, `user_id`, `created_at`, `team_id`) VALUES (1, 'So, what\'s the best way to kill a wyvern?', 1, 3, '2022-06-22 09:22:22', 1);
-INSERT INTO `chat` (`id`, `text`, `chat_id`, `user_id`, `created_at`, `team_id`) VALUES (2, 'Rip its head off?', 1, 2, '2022-06-22 10:22:22', 1);
+INSERT INTO `chat` (`id`, `content`, `user_id`, `created_at`, `team_id`) VALUES (1, 'So, what\'s the best way to kill a wyvern?', 3, '2022-06-22 09:22:22', 1);
+INSERT INTO `chat` (`id`, `content`, `user_id`, `created_at`, `team_id`) VALUES (2, 'Rip its head off?', 2, '2022-06-22 10:22:22', 1);
 
 COMMIT;
 

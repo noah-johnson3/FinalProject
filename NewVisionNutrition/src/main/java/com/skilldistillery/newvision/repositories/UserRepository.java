ba4c432a776cc7b906df9ab190ffa65a1d001311
noah-johnson3 <noah.johnson3@yahoo.com>
2022-06-23 10:40:25 -0400

    Persistence XML Stuff

--- NewVisionNutrition/src/main/java/com/skilldistillery/newvision/repositories/UserRepository.java
+++ NewVisionNutrition/src/main/java/com/skilldistillery/newvision/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.skilldistillery.newvision.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.skilldistillery.newvision.entities.User;
 
 public interface UserRepository  extends JpaRepository<User, Integer>{

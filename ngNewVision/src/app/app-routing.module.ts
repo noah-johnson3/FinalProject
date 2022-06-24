3bdcc06aa549955a58ff37b724de7bb7292eb72a
Katherine <remick.ka@gmail.com>
2022-06-24 12:42:54 -0700

    navbar, login functional

--- ngNewVision/src/app/app-routing.module.ts
+++ ngNewVision/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { UserComponent } from './components/user/user.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { BlogComponent } from './components/blog/blog.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'user', component: UserComponent },
+  {path: 'register' , component: RegisterComponent},
+  {path: 'login' , component: LoginComponent}
+  //{path: '**' , component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ngNewVision/src/app/components/navbar/navbar.component.ts
+++ ngNewVision/src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit(): void {
+  }
+  loggedIn(): boolean{
+    return this.auth.checkLogin();
   }
 
 }

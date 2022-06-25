fe521950923ad87efc5bf720bfdf3c1cc4e2db5a
noah-johnson3 <noah.johnson3@yahoo.com>
2022-06-25 11:27:16 -0400

    Starting on hmtl for recipe page

--- ngNewVision/src/app/components/recipes/recipes.component.ts
+++ ngNewVision/src/app/components/recipes/recipes.component.ts
@@ -1,5 +1,7 @@
-import { RecipeService } from './../../services/recipe.service';
+
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe';
+import { RecipeService } from './../../services/recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -8,18 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  allRecipes: Recipe [] = [];
+  arrayOfRecipeArray: Recipe [] [] = [];
+
 
   //*************************** Setup ******************** */
 
   constructor(private recipeServ: RecipeService) { }
 
   ngOnInit(): void {
+    this.getRecipes();
   }
 
 
   //*************************** Page Dynamics ******************** */
+  sortRecipes(){
+    while(this.allRecipes.length > 0){
+      let tempArray: Recipe[] = [];
+      for(let x = 0; x < 3 && this.allRecipes.length > 0; x++){
+        let recipeHeld: Recipe | undefined = this.allRecipes.pop();
+        if(recipeHeld){
+          tempArray.push(recipeHeld);
+        }
+      }
+      this.arrayOfRecipeArray.push(tempArray);
 
+    }
 
+  }
 
 
 
@@ -27,5 +45,17 @@ export class RecipesComponent implements OnInit {
 
   //*************************** Service Methods ******************** */
 
+  getRecipes(){
+    this.recipeServ.listAllRecipes().subscribe({
+      next: (recipeArray) => {
+        this.allRecipes = recipeArray;
+        this.sortRecipes();
+      },
+      error: (problem) => {
+        console.error('getRecipes(): error loading recipe list:');
+        console.error(problem);
+      }
+    });
+  }
 
 }

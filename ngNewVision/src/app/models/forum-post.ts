8e14a07a6695fb3971e0ce016d236ebc242f7c37
Katherine <remick.ka@gmail.com>
2022-06-29 09:01:28 -0700

    forum post service complete

--- ngNewVision/src/app/models/forum-post.ts
+++ ngNewVision/src/app/models/forum-post.ts
@@ -1,2 +1,25 @@
+import { User } from "./user";
+
 export class ForumPost {
+
+  id: number;
+  createdAt: Date | null;
+  inReplyTo: ForumPost | null;
+  responses: ForumPost [];
+  user : User | null;
+
+  constructor(
+    id: number = 0,
+  createdAt: Date | null= null,
+  inReplyTo: ForumPost | null = null,
+  responses: ForumPost [] = [],
+  user : User | null = null
+  ){
+    this.id = id;
+    this.createdAt = createdAt;
+    this.inReplyTo = inReplyTo;
+    this.responses = responses;
+    this.user = user;
+  }
+
 }
--- ngNewVision/src/app/services/forum.service.ts
+++ ngNewVision/src/app/services/forum.service.ts
@@ -1,9 +1,90 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ForumPost } from '../models/forum-post';
+import { Goal } from '../models/goal';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForumService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/forums';
+
+
+
+  constructor( private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  getParentForums(): Observable<ForumPost[]> {
+    return this.http.get<ForumPost[]>(this.url).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
+  getParentForumsByTopic(topic: string): Observable<ForumPost[]> {
+    return this.http.get<ForumPost[]>(this.url + "/topic/" + topic).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
+  getForumResponses(id: number): Observable<ForumPost[]> {
+    return this.http.get<ForumPost[]>(this.url + "/responses/" + id).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
+  createForum(post :ForumPost): Observable<ForumPost> {
+    return this.http.post<ForumPost>(this.url , post,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
+  updateForum(post :ForumPost, id: number): Observable<ForumPost> {
+    return this.http.put<ForumPost>(this.url + "/" + id , post,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
+  deleteForum(id: number): Observable<ForumPost> {
+    return this.http.put<ForumPost>(this.url + "/" + id ,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('index goal error: ' + err)
+      );
+      })
+    );
+  }
 }
